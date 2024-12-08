9b8b95af5901132f4cd1c5621de16631600a9400
kurokyuu1 <16856673+kurokyuu1@users.noreply.github.com>
2024-12-08 05:46:06 +0100

    day five and six

--- AdventOfCode/2024/Puzzles/DaySix/SolutionDaySix.cs
+++ AdventOfCode/2024/Puzzles/DaySix/SolutionDaySix.cs
@@ -1,6 +1,32 @@
-﻿namespace AdventOfCode._2024.Puzzles.DaySix;
+﻿using System.Collections.Specialized;
+using AdventOfCode.Core.Enumeration;
 
-public class SolutionDaySix
+namespace AdventOfCode._2024.Puzzles.DaySix;
+
+[AdventModule("Day Six")]
+public sealed class SolutionDaySix : SolutionBase
 {
-    
+    public SolutionDaySix() : base("2024", "07", true)
+    {
+    }
+
+    private const string SpaceSeparator = " ";
+
+    public override async Task RunAsync()
+    {
+        var lines = await InternalReadAllLinesAsync(ReadingMode.TestInput);
+
+        var sum = lines.Sum(SolveEquation);
+
+        PuzzleOneResult(sum);
+    }
+
+    private int SolveEquation(string line)
+    {
+        var split = line.Split(":");
+        var result = split[0];
+        var numbers = split[1].Split(SpaceSeparator, StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        var counter = 0;
+        return counter;
+    }
 }
